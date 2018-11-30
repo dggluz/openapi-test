bca4ff58d110cc5b8671539a026890942c065d2a
Gonza Gluzman <dggluz@gmail.com>
2018-11-30 20:08:57 -0300

    feat(server): first commit

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,83 @@
-import * as os from 'os';
-import {readFile} from 'fs';
-import {add} from './foo/foo';
+import { createServer, plugins } from 'restify';
+import { strictObjOf, num, str, optional, ParmenidesError, Contract, arrOf } from 'parmenides';
 
-readFile('./package.json', (err, packageStr) => {
-    if (err) {
-        console.error('There was a problem reading package json', err);
-        return;
+const petContract = strictObjOf({
+    id: num,
+    name: str,
+    tag: optional(str)
+});
+
+const newPetContract = strictObjOf({
+    name: str,
+    tag: optional(str)
+});
+
+var server = createServer();
+server.use(plugins.bodyParser());
+
+server.get('/pets', (_req, res) => {
+    res.send(pets);
+});
+
+server.get('/pets/:petId', (req, res) => {
+    if (isNaN(req.params && req.params.petId)) {
+        return res.send(400, {
+            message: 'Invalid petId'
+        });
     }
 
-    const json = JSON.parse(packageStr.toString());
+    const pet = pets.find(aPet => aPet.id === Number(req.params.petId));
 
-    console.log(`Running typescript-node-starter version ${json.version}`);
-    console.info(`Running on ${os.hostname()} with ${os.cpus().length} CPU's and ${os.totalmem()} mem`);
-    console.info(`1 + 1 = ${add(1, 1)}`);
+    if (!pet) {
+        return res.send(404, {
+            message: `Pet with id ${req.params.id} does not exist`
+        });
+    }
+
+    res.send(pet);
+});
+
+server.get('/ping', (_req, res) => {
+    res.send({});
+});
+
+server.post('/pets', (req, res) => {
+    try {
+        const pet = {
+            ...newPetContract(req.body),
+            id: getLastPetId(pets) + 1
+        };
+        pets.push(pet);
+
+        res.send(pet);
+    }
+    catch(err) {
+        if (err instanceof ParmenidesError) {
+            res.send(400, {message: err.message});
+        }
+        else {
+            throw err;
+        }
+    }
+});
+
+const getLastPetId = <T extends {id: number}>(pets: T[]) =>
+    pets
+        .map(pet => pet.id)
+        .sort((a, b) => b - a)
+        [0] || 0
+;
+
+const pets = arrOf(petContract)([{
+    id: 1,
+    name: 'Bobby',
+    tag: 'crazy'
+}, {
+    id: 2,
+    name: 'Manuelita',
+    tag: 'slow'
+}]);
+
+server.listen(3000, function() {
+  console.log('%s listening at %s', server.name, server.url);
 });
